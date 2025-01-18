4e7fe28fc2397abe2e623a7e042d97cbddea3d7e
SeanTerrr <e1122380@u.nus.edu>
2025-01-18 22:35:35 +0800

    Increment Level 0

---
+++ src/main/java/Mon.java
@@ -0,0 +1,11 @@
+public class Mon {
+    public static void main(String[] args) {
+        String botName = "Mon";
+        String horizontalLine = "_______________________________";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + botName + "\n" + "What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
