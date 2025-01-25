9dc21bd2d559ff7493475521ad73be4a796bfa9f
SeanTerrr <e1122380@u.nus.edu>
2025-01-25 16:44:46 +0800

    Updated code to allow Mon to echo

--- src/main/java/Mon.java
+++ src/main/java/Mon.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Mon {
     public static void main(String[] args) {
         String botName = "Mon";
-        String horizontalLine = "_______________________________";
+        String horizontalLine = "   _______________________________";
         System.out.println(horizontalLine);
-        System.out.println("Hello! I'm " + botName + "\n" + "What can I do for you?");
+        System.out.println("    Hello! I'm " + botName + "\n" + "    What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String userInput = in.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(horizontalLine);
+                System.out.println("    " + userInput);
+                System.out.println(horizontalLine);
+            }
+        }
+        System.out.println(horizontalLine);
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
 }
